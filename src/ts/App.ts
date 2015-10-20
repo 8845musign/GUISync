17496ac4950b19755c7ed65b2d3a3aa90496c7b3
8845musign <8845musign@gmail.com>
2015-10-21 07:56:12 +0900

    working save.

--- src/ts/App.ts
+++ src/ts/App.ts
@@ -1,12 +1,17 @@
 /// <reference path="../../typings/github-electron/github-electron-renderer.d.ts" />;
 /// <reference path="../../typings/bluebird/bluebird.d.ts" />;
 
+// Services
 import BrowserSyncService = require('./lib/modules/services/BrowserSyncService');
 import SettingModel       = require('./lib/modules/models/SettingModel');
 import ConsoleService     = require('./lib/modules/services/consoleService');
 import SettingFileService = require('./lib/modules/services/SettingFileService');
+// UI
 import Dialog             = require('./lib/modules/ui/dialog/dialog');
 import Loading            = require('./lib/modules/ui/loading/Loading');
+
+//View
+import BtnActionView = require('./lib/modules/views/BtnActionView');
 
 class App {
   private btnStart:HTMLButtonElement;
@@ -20,21 +25,34 @@ class App {
   }
 
   public start() : void {
-    this.btnStart   = <HTMLButtonElement>document.getElementById('btnStart');
-    this.btnInstall = <HTMLButtonElement>document.getElementById('btnModuleInstall');
-    this.btnStop    = <HTMLButtonElement>document.getElementById('btnStop');
+    var btnActionView = new BtnActionView({
+      el:'#btnRun',
+      startFunc: function(event) {
+        if (this.browserSync.isRunning() === true) return;
+        this.createSettingFile();
+  
+        var start = this.browserSync.start();
+
+        start.on('start', function(data){
+          this.console.log(data);
+        });
+  
+        start.on('error', function(data){
+          this.console.log(data);
+        });
+  
+        event.emit('started');
+      }.bind(this),
+      stopFunc: function(event){
+        this.browserSync.stop();
+        event.emit('stoped');
+      }.bind(this),
+    });
     this.console    = new ConsoleService('#windowConsole');
 
-    this.bind();
     this.checkBrowserSync();
   }
 
-  public bind() : void {
-    this.btnStart.addEventListener('click', this.onClickStartBtn.bind(this));
-    this.btnInstall.addEventListener('click', this.onClickInstallBtn.bind(this));
-    this.btnStop.addEventListener('click', this.onClickStopBtn.bind(this));
-  }
-  
   public checkBrowserSync() {
     BrowserSyncService.isInstelled().then(function(){
       // nothing
@@ -51,42 +69,7 @@ class App {
       });
     });
   }
-
-  private onClickStartBtn(e:Event) : void {
-    if (this.browserSync.isRunning() === true) return;
-
-      this.createSettingFile();
-
-      var start = this.browserSync.start();
-
-      start.on('start', function(data){
-        this.console.log(data);
-      }.bind(this));
-
-      start.on('error', function(data){
-        this.console.log(data);
-      }.bind(this));
-
-      this.btnStart.disabled = false;
-      this.btnStop.disabled = true;
-
-      e.preventDefault();
-  }
-  
-  private onClickInstallBtn(e:Event) : void {
-    var install = BrowserSyncService.install();
-
-    install.on('install', function(data){
-      this.console.log(data);
-      this.btnInstall.disabled = false;
-    }.bind(this));
-
-    install.on('error', function(data){
-      this.console.log(data);
-      this.btnInstall.disabled = false;
-    }.bind(this));
-  }
-  
+    
   private onClickStopBtn(e:Event) : void {
     if (this.browserSync.isRunning() === false) return;
     this.browserSync.stop();
---
+++ src/ts/lib/modules/views/BtnActionView.ts
@@ -0,0 +1,79 @@
+/// <reference path="../../../../../typings/github-electron/github-electron-renderer.d.ts" />;
+var EventEmitter = require('events').EventEmitter;
+
+class BtnActionView extends EventEmitter {
+    private el:HTMLElement;
+    private startFunc:Function;
+    private stopFunc:Function;
+    private mode:Mode;
+
+    /**
+     * constructor
+     */
+    constructor(options:{selector:string, startFunc:Function, stopFunc:Function}) {
+        super();
+        this.el         = <HTMLElement>document.querySelector(options.selector);
+        this.startFunc  = options.startFunc;
+        this.stopFunc   = options.stopFunc;
+
+        this.init();
+    }
+
+    /**
+     * initialize
+     */
+    private init():void {
+        this.mode = Mode.start;
+        this.el.classList.add(Const.CSS_CLASS_START);
+
+        this.el.addEventListener('click', this.onClick.bind(this));
+    }
+	
+    /**
+     * on click
+     */
+    private onClick(e:Event):void {
+        if (this.mode === Mode.start) {
+            this.start();
+        } else {
+            this.stop();
+        }
+
+        e.preventDefault();
+    }
+
+    /**
+     * start function
+     */
+    private start():void {
+        this.startFunc(this);
+        this.el.classList.remove(Const.CSS_CLASS_START);
+        this.el.classList.add(Const.CSS_CLASS_STOP);
+    }
+	
+    /**
+     * stop function
+     */
+    private stop():void {
+        this.stopFunc(this);
+        this.el.classList.remove(Const.CSS_CLASS_STOP);
+        this.el.classList.add(Const.CSS_CLASS_START);
+    }
+}
+export = ActionBtn;
+
+/**
+ * Button Mode
+ */
+enum Mode{
+	start,
+	stop
+}
+
+/**
+ * const
+ */
+module Const {
+    export const CSS_CLASS_START    = 'btnAction--start';
+    export const CSS_CLASS_STOP     = 'btnAction--stop';
+}
